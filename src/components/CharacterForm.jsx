8c0503bc3d0825032d5db975396c39ed8c85cdfd
Benoit22324 <sunbenoit384@gmail.com>
2024-11-21 15:18:28 +0100

    Adding a Error Message

--- src/components/CharacterForm.jsx
+++ src/components/CharacterForm.jsx
@@ -3,18 +3,21 @@ import { selectCharacter, selectCharactersList } from "../../store/selector/char
 import { setSelectedCharacter } from "../../store/reducer/CharacterReducer";
 import { useNavigate } from "react-router-dom";
 import { uncleanName } from "../utils/uncleanName";
+import { useState } from "react";
 
 export const CharacterForm = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const selectedCharacter = useSelector(selectCharacter);
     const charactersList = useSelector(selectCharactersList);
+    const [error, setError] = useState("");
 
     const handleValue = (e) => {
         const value = e.target.value;
 
         if (value.trim() !== "" || value.trim() === "") {
             dispatch(setSelectedCharacter(value));
+            setError("");
         }
     }
 
@@ -28,10 +31,19 @@ export const CharacterForm = () => {
         if (selectedCharacter.trim() !== "" && existInDataBase) {
             navigate(`/reactexo1final/character/${revertName}`);
         }
+        else if (selectedCharacter.trim() === "" && !existInDataBase) {
+            setError("Veuillez saisir le nom d'un personnage");
+        }
+        else {
+            setError("Le personnage que vous cherchez n'existe pas");
+        }
     }
 
     return <>
         <h2>Quick Search</h2>
+        {
+            error !== "" && <p className="errorMessage">{error}</p>
+        }
         <form className="characterForm" onSubmit={(e) => handleSubmit(e)}>
             <input className="characterInput" type="text" value={selectedCharacter} onChange={(e) => handleValue(e)} />
             <button className="characterSubmit">Search</button>
--- src/pages/Credit.jsx
+++ src/pages/Credit.jsx
@@ -1,8 +1,14 @@
+import {useState } from "react"
+
 export const Credit = () => {
+    const [cheers, setCheers] = useState(0);
+
     return <>
-        <h1>Credit Page</h1>
+        <h1>Credit</h1>
         <p>Api Genshin : https://genshin.jmp.blue/</p>
         <p>Made by : Marvin "NurMarvin" Witt</p>
         <p>Link to the API page : <a href="https://genshin.dev">https://genshin.dev</a></p>
+        <button className="cheerButton" onClick={() => setCheers(cheers + 1)}>Cheer for it</button>
+        <p>Number of time you cheered : {cheers}</p>
     </>
 }
